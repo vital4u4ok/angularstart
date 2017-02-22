d36d4463175b24d8639c1b41e44cbaddb64351f7
vital4u4ok <vital4u4ok@gmail.com>
2017-02-22 17:26:37 +0200

    added login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,30 @@
 import {Component} from '@angular/core';
+import {LoginService} from "./login.service";
+import {Observable} from "rxjs";
+import {User} from "./model/user";
 
 @Component({
   moduleId: module.id,
   selector: 'login-form',
-  templateUrl: './login.component.html'
+  templateUrl: 'login.component.html'
 })
 
 export class LoginComponent {
+  constructor(
+    private loginService: LoginService
+  ){}
 
+  private model = new User(-1, '', '');
+
+  onSubmit(){
+    let loginData:Observable<User>;
+
+    loginData = this.loginService.getUser(new User(-1, "", ""));
+
+    loginData.subscribe(result => this.checkResult(result));
+  }
+
+  checkResult(user: User){
+
+  }
 }
