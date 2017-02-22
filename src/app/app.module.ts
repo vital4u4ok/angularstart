22f07b2e0daf59f8dff02d5497cf9fc1c18723dd
vital4u4ok <vital4u4ok@gmail.com>
2017-02-22 17:27:41 +0200

    app module and app component changes related to the login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,29 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule }  from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import {LoginComponent} from "./login/login.component";
+import {HttpModule} from "@angular/http";
+import {LoginService} from "./login/login.service";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ])
+  ],
+  declarations: [ AppComponent, LoginComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [
+    LoginService
+  ],
 })
 export class AppModule { }
