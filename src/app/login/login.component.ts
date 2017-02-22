42d4de9b5d2309d4e17b0b8ed6e1f90b9f4423e8
vital4u4ok <vital4u4ok@gmail.com>
2017-02-22 17:24:25 +0200

    user model

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'login-form',
+  templateUrl: './login.component.html'
+})
+
+export class LoginComponent {
+
+}
